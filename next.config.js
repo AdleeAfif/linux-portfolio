f2c7b85ca422cbf597603e2bb1d8514d7d7e72e7
AdleeAfif <kamitokazuya@gmail.com>
2025-06-25 14:14:39 +0800

    add next config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "./",
+  basePath: "/linux-portfolio",
+};
+
+module.exports = nextConfig;
